182fa291dab6a6029a94c2148a3dba6617ae9e73
Tonmoy-Roy <emonismail69@gmail.com>
2022-08-28 14:59:33 +0600

    first commit

---
+++ functional bank/funtional.js
@@ -0,0 +1,9 @@
+function getInputFieldValueById(inputId){
+    const inputField=document.getElementById(inputId);
+    const inputValueString=inputField.value;
+    return inputValueString;
+}
+document.getElementById('btn-deposite').addEventListener('click', function() {
+   
+}
+)
